f75787b1d144778f0e3bfa7775e675f322e7cb75
dev-rfgul <rfgul-587@gmail.com>
2024-08-09 17:39:02 +0500

    .

--- 02/tuple.ts
+++ 02/tuple.ts
@@ -17,3 +17,5 @@ newUser[1] = 'fahad';
 
 // its an edge case of a tuple that the tuple array being defined by the length of 02 can still perform push, pop and other array methords
 newUser.push (1);
+
+//added a dummy comment 

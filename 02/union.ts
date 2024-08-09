635d1e5d95501fe7047be1bdd621066f273edb2c
rf-gul <rf-gul@pop-os.localdomain>
2024-08-09 16:42:39 +0500

    .

--- 02/union.ts
+++ 02/union.ts
@@ -1,3 +1,31 @@
 let number :number| string = 10;
 
 //that's how we can use union type in typescript to mix two or more data types in a single variable.
+
+
+number = "ten";
+number =12;
+
+//both are allowed when we have both the types in union type.
+
+type user={
+    name:string;
+    id:number;
+}
+
+type admin ={
+    userName:string;
+    id:number;
+    role:string;
+}
+
+let fahad:user|admin = {
+    name:"fahad",
+    id:785,
+}
+
+fahad={userName:"fahad",id:785,role:"admin"};  
+
+const data:number []=[1,2,3,4,5,6,7,8,9,10];
+const data2:string[]=["1","2","3","4","5","6","7","8","9","10"];
+const data3:(number | string | boolean)[]=[1,2,3,4,5,6,7,8,9,10,"1","2","3","4","5","6","7","8","9","10", true, false]; 

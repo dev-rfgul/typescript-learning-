a0a92383dda38d3beea4dbfa991ff0d4ab00dd29
dev-rfgul <rfgul-587@gmail.com>
2024-08-09 17:34:46 +0500

    removing errors

--- 02/tuple.js
+++
@@ -1,9 +0,0 @@
-var user = ['John', 1];
-var tUser;
-tUser = ['John', 1, true];
-// we cant change the order of the tuple    
-// rgb is the best example of tuple  
-var rgb = [255, 0, 0];
-var newUser = [1, 'John'];
-newUser[1] = 'fahad';
-newUser.push(1);
